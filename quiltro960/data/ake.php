0e79dc80268779e2db7b550ea10ff9ec9aeb9b21
ideoeduardo <ideonapi@gmail.com>
2024-07-08 14:10:25 -0400

    sitio-motools
    sitio-motools

---
+++ quiltro960/data/ake.php
@@ -0,0 +1,34 @@
+
+
+
+
+<?php 
+//mi_pagina aka.php
+header('Content-Type: text/html; charset=utf-8');
+header ("Cache-Control: no-cache, must-revalidate ");
+?>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro960/img/photo_60x60.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro960/img/photo_60x60.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div></hr>
+<!--nota01-->
+<!--nota01--></hr>
+<!--nota01-->
+<!--nota01--></hr>
+<!--nota01-->
